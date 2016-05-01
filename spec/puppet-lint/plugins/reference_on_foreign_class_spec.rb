f2c518cce7de806e469e7d543cad3fdc9a2c024c
Martin Alfke <ma@example42.com>
2016-05-01 15:10:13 +0200

    update gitignore to new name for gem files
    add more spec tests

--- spec/puppet-lint/plugins/reference_on_foreign_class_spec.rb
+++ spec/puppet-lint/plugins/reference_on_foreign_class_spec.rb
@@ -16,12 +16,40 @@ describe 'reference_on_foreign_class' do
     end
   end
 
+  context 'a proper reference on a local class include using a variable' do
+    let(:code) { "
+      class demo {
+        include $install
+        include $config
+        Class[$config] -> Class[$install]
+      }
+            " }
+
+    it 'should detect no problem' do
+      expect(problems).to have(0).problem
+    end
+  end
+
   context 'a proper reference on a local class type declaration' do
     let(:code) { "
       class demo2 {
         class { demo2::install: }
         class { demo2::config: }
         Class['demo2::config'] -> Class['demo2::install']
+      }
+            " }
+
+    it 'should detect no problem' do
+      expect(problems).to have(0).problem
+    end
+  end
+
+  context 'a proper reference on a local class type declaration using a variable' do
+    let(:code) { "
+      class demo2 {
+        class { $install: }
+        class { $config: }
+        Class[$config] -> Class[$install]
       }
             " }
 
@@ -38,7 +66,39 @@ describe 'reference_on_foreign_class' do
       Class['demo'] -> Class['foo']
             " }
 
-    it 'should only detect a single problem' do
+    it 'should detect two problems' do
+      expect(problems).to have(2).problem
+    end
+
+    it 'should create a warning' do
+      expect(problems).to contain_warning(msg)
+    end
+  end
+
+  context 'a reference on a foreign class declaration using a variable' do
+    let(:msg) { 'reference on a foreign class' }
+    let(:code) { "
+      include $base::demo
+      Class[$demo] -> Class[$foo]
+            " }
+
+    it 'should detect two problems' do
+      expect(problems).to have(2).problem
+    end
+
+    it 'should create a warning' do
+      expect(problems).to contain_warning(msg)
+    end
+  end
+
+  context 'a reference on a foreign class type declaration' do
+    let(:msg) { 'reference on a foreign class' }
+    let(:code) { "
+      class{ base::demo: }
+      Class['demo'] -> Class['foo']
+            " }
+
+    it 'should detect two problems' do
       expect(problems).to have(2).problem
     end
 
